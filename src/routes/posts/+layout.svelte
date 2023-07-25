50b3b0fecdf0422742c8ae4d164025ca6a09e649
evariste1963 <evariste@hotmail.co.uk>
2023-07-25 21:58:22 +0100

    1:18:00 => dynamic and optional parameters --> up to rest parameters

---
+++ src/routes/posts/+layout.svelte
@@ -0,0 +1,29 @@
+<div class="post">
+	<aside>
+		<h4>Posts</h4>
+
+		<nav>
+			<ul>
+				<li>
+					<a href="/posts/one">One</a>
+				</li>
+				<li>
+					<a href="/posts/two">Two</a>
+				</li>
+			</ul>
+		</nav>
+	</aside>
+
+	<main>
+		<slot />
+	</main>
+</div>
+
+<style>
+	.post {
+		display: grid;
+		gap: 2rem;
+		grid-template-columns: 200px 60ch;
+		margin-top: 2rem;
+	}
+</style>
---
+++ src/routes/posts/create/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Create new Post</h1>
+
+<p>Create a new Post</p>
