2149db95e43e3c0606c248dc6cf1a4bad74ad1fe
evariste1963 <evariste@hotmail.co.uk>
2023-07-26 09:23:47 +0100

    more commenting

--- src/params/matcher.js
+++ src/params/matcher.js
@@ -1,4 +1,5 @@
-// matching url slug using Matcher & regex  [[slug=matcher]] which in this instance uses matcher.js file in src dir -- lowercase a-z and 0-9 allowed here--> folder/file names are arbitry//
+/* matching url slug using Matcher & regex  [[slug=matcher]] which in this instance uses matcher.js file in src dir -- lowercase a-z and 0-9  and - allowed here
+--> folder/file names are arbitry*/
 
 export const match = (param) => {
 	return /^[a-z0-9-]+$/.test(param);
--- src/routes/posts/[slug=matcher]/+page.svelte
+++ src/routes/posts/[slug=matcher]/+page.svelte
@@ -1,4 +1,7 @@
 <script>
+    /* matching url slug using Matcher & regex  [[slug=matcher]] which in this instance uses matcher.js file in src dir -- lowercase a-z and 0-9  and - allowed here
+--> folder/file names are arbitry*/
+
 	import { page } from '$app/stores';
 </script>
 
