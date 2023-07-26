3450145d0a4d1743e4edc589b1d8c19a9298532d
evariste1963 <evariste@hotmail.co.uk>
2023-07-26 09:03:17 +0100

    some commenting

--- src/params/matcher.js
+++ src/params/matcher.js
@@ -1,4 +1,4 @@
-//import { ParamMatcher } from '@sveltejs/kit';
+// matching url slug using Matcher & regex  [[slug=matcher]] which in this instance uses matcher.js file in src dir -- lowercase a-z and 0-9 allowed here--> folder/file names are arbitry//
 
 export const match = (param) => {
 	return /^[a-z0-9-]+$/.test(param);
--- src/routes/media/[...file]/[width]/[height]/+page.svelte
+++ src/routes/media/[...file]/[width]/[height]/+page.svelte
@@ -1,6 +1,7 @@
 <script>
+	//REST parameters for variable and unknown number of paramters (in url path)//
 	import { page } from '$app/stores';
-	import { json } from '@sveltejs/kit';
+	
 </script>
 
 <h1>Rest parameters</h1>
