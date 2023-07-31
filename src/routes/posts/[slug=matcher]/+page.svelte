62d3fa0b49382c4268da5aa695c7b7224dd74bad
evariste1963 <evariste@hotmail.co.uk>
2023-07-31 07:01:16 +0100

    1st part of loading data om server

--- src/routes/posts/[slug=matcher]/+page.svelte
+++ src/routes/posts/[slug=matcher]/+page.svelte
@@ -2,12 +2,12 @@
 	/* matching url slug using Matcher & regex  [[slug=matcher]] which in this instance uses matcher.js file in src dir -- lowercase a-z and 0-9  and - allowed here
 --> folder/file names are arbitry*/
 
-	import { page } from '$app/stores';
-	const titleFromSlug = $page.params.slug.split('-').join(' ');
+	// 	import { page } from '$app/stores';
+	// 	const titleFromSlug = $page.params.slug.split('-').join(' ');
+	//
+
+	export let data;
+	console.log(data);
 </script>
 
-<h1>{titleFromSlug}</h1>
-
-<pre>
-    {JSON.stringify($page, null, 2)}
-</pre>
+<h1>{data.post.title}</h1>
