c033bb8bf103eba9a43864c788f0b7d52cf7ac4f
evariste1963 <evariste@hotmail.co.uk>
2023-08-01 11:26:14 +0100

    [slug=matcher] correction in comments

--- src/params/matcher.js
+++ src/params/matcher.js
@@ -1,4 +1,4 @@
-/* matching url slug using Matcher & regex  [[slug=matcher]] which in this instance uses matcher.js file in src dir -- lowercase a-z and 0-9  and - allowed here
+/* matching url slug using Matcher & regex  [slug=matcher] which in this instance uses matcher.js file in src/params dir -- lowercase a-z and 0-9  and - allowed here
 --> folder/file names are arbitry*/
 
 export const match = (param) => {
