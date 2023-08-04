f04e2a53ee998ed933f6a0bd5ee7e300abaeac45
evariste1963 <evariste@hotmail.co.uk>
2023-08-04 08:47:16 +0100

    api changes __> limit & order

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,5 +1,13 @@
+const limit = Math.round(Math.random() * 30);
+
 export const load = async ({ fetch }) => {
-	const response = await fetch('/api/posts');
+	const response = await fetch(`/api/posts?limit=${limit}`); //see below for other params
 	const posts = await response.json();
 	return { posts };
 };
+
+//in order to change number of posts and limit
+//you can define the searchParams once they are set up as api/+server.js variables -> eg
+//const response = await fetch('/api/posts?limit=10&order=desc');
+
+//limit and order (orderBy) are standard db query params
--- src/routes/api/posts/+server.js
+++ src/routes/api/posts/+server.js
@@ -1,7 +1,9 @@
-import db from '../../../lib/database.js';
+import db from '$lib/database.js';
 import { json } from '@sveltejs/kit';
 
 /** @type {import('./$types').RequestHandler} */
+/*
+//sveltekit (or any framework 'state anagement)
 export const GET = async (event) => {
 	const posts = await db.post.findMany({
 		take: Math.round(Math.random() * 30)
@@ -9,6 +11,20 @@ export const GET = async (event) => {
 
 	event.setHeaders({
 		'Cache-Control': 'max-age=60'
+	});
+
+	return json(posts);
+};
+*/
+
+//Alternative (old way) JS URL 'state management' used without js framework
+export const GET = async ({ url }) => {
+	const limit = +(url.searchParams.get('limit') ?? 30);
+	const order = url.searchParams.get('order') ?? 'asc';
+
+	const posts = await db.post.findMany({
+		orderBy: { id: order },
+		take: limit
 	});
 
 	return json(posts);
